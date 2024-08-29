a0e6a9aa8cd6d18624971c8e6ede99112ab7bd00
Htet-Shine-Htwe <htetshine.coin@gmail.com>
2024-08-29 16:22:52 +0630

    dev: add test for product Module Test

--- tests/SupportTraits/Authenticated.php
+++ tests/SupportTraits/Authenticated.php
@@ -3,30 +3,64 @@
 namespace Tests\SupportTraits;
 
 use App\Enums\RoleEnum;
+use App\Models\User;
 use Database\Seeders\RolePermissionSeeder;
+use Spatie\Permission\Models\Role;
 
-trait Authenticated
-{
-    public function setUp(): void
-    {
-        parent::setUp();
+trait Authenticated {
 
-        $this->seed(RolePermissionSeeder::class);
-        $this->registerAdmin();
+    protected $admin;
+    protected $user;
+
+
+    /**
+    * Seed the given seeder classes.
+    *
+    * @param string[] ...$seederClasses
+    * @return void
+    */
+    public function seedClasses( ...$seederClasses ) {
+        foreach ( $seederClasses as $seederClass ) {
+            $this->artisan( 'db:seed', [
+                '--class' => $seederClass,
+            ] );
+        }
     }
 
-    public function registerAdmin(): void
-    {
-       $this->admin = \App\Models\User::factory()->create()->assignRole(enum_value(RoleEnum::ADMIN));
+    /**
+    * Register and create an admin user.
+    */
+    protected function registerAdmin(): void {
+        $this->admin = User::factory()->create();
+        $this->admin->assignRole( enum_value( RoleEnum::ADMIN ) );
     }
 
-    public function actingAsUser(): void
-    {
-        $this->actingAs(\App\Models\User::factory()->create());
+    /**
+    * Set the acting user as a regular user.
+    *
+    * @return $this
+    */
+
+    public function actingAsUser() : self {
+        $this->user = User::factory()->create();
+        $this->actingAs( $this->user );
+
+        return $this;
     }
 
-    public function actingAsAdmin(): void
-    {
-        $this->actingAs($this->admin);
+    /**
+    * Set the acting user as an admin.
+    *
+    * @return $this
+    */
+
+    public function actingAsAdmin() : self {
+        if ( !$this->admin ) {
+            $this->registerAdmin();
+        }
+
+        $this->actingAs( $this->admin );
+
+        return $this;
     }
 }
