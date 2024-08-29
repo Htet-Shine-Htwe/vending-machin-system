5b7c666986fe00aa16828c2975f08a4f5b43a0a4
Htet-Shine-Htwe <htetshine.coin@gmail.com>
2024-08-29 09:08:08 +0630

    dev : product ui components

--- resources/views/components/product-card.blade.php
+++ resources/views/components/product-card.blade.php
@@ -2,54 +2,81 @@
 
 <div class="rounded-lg border border-gray-200 bg-white p-6 shadow-sm dark:border-gray-700 dark:bg-gray-800">
     <div class="h-56 w-full">
-      <a href="#">
-            <img src="{{ $product?->image }}" alt="Product image" class="object-cover object-center w-full h-full rounded-lg" />
-      </a>
+        <a href="#">
+            <img src="{{ $product?->image }}" alt="Product image"
+                class="object-cover object-center w-full h-full rounded-lg" />
+        </a>
     </div>
     <div class="pt-6">
-      <div class="mb-4 flex items-center justify-between gap-2">
+        <div class="mb-4 flex items-center justify-between gap-2">
 
-        <div class="flex items-center justify-end gap-4">
-          <button type="button" data-tooltip-target="tooltip-quick-look" class="">
-            <span class="sr-only"> Quick look </span>
-            <svg class="h-5 w-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-              <path stroke="currentColor" stroke-width="2" d="M21 12c0 1.2-4.03 6-9 6s-9-4.8-9-6c0-1.2 4.03-6 9-6s9 4.8 9 6Z" />
-              <path stroke="currentColor" stroke-width="2" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-            </svg>
-          </button>
-          <div id="tooltip-quick-look" role="tooltip" class="tooltip invisible absolute z-10 inline-block rounded-lg bg-gray-900 px-3 py-2 text-sm font-medium text-white opacity-0 shadow-sm transition-opacity duration-300 dark:bg-gray-700" data-popper-placement="top">
-            Quick look
-            <div class="tooltip-arrow" data-popper-arrow=""></div>
-          </div>
+            <div class="flex items-center justify-end gap-4">
+                <a href="{{ route('products.edit', $product->slug) }}" data-tooltip-target="tooltip-quick-look"
+                    class="">
+                    <span class="sr-only"> Quick look </span>
+                    <svg class="h-5 w-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24"
+                        height="24" fill="none" viewBox="0 0 24 24">
+                        <path stroke="currentColor" stroke-width="2"
+                            d="M21 12c0 1.2-4.03 6-9 6s-9-4.8-9-6c0-1.2 4.03-6 9-6s9 4.8 9 6Z" />
+                        <path stroke="currentColor" stroke-width="2" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                    </svg>
+                </a>
+                <div id="tooltip-quick-look" role="tooltip"
+                    class="tooltip invisible absolute z-10 inline-block rounded-lg bg-gray-900 px-3 py-2 text-sm font-medium text-white opacity-0 shadow-sm transition-opacity duration-300 dark:bg-gray-700"
+                    data-popper-placement="top">
+                    Quick look
+                    <div class="tooltip-arrow" data-popper-arrow=""></div>
+                </div>
 
-          <button type="button" data-tooltip-target="tooltip-add-to-favorites" class="mb-1">
-            <span class="sr-only"> Delete </span>
-            <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0,0,300,150">
-                <g fill="#cf0c0c" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><g transform="scale(8.53333,8.53333)"><path d="M14.98438,2.48633c-0.55152,0.00862 -0.99193,0.46214 -0.98437,1.01367v0.5h-5.5c-0.26757,-0.00363 -0.52543,0.10012 -0.71593,0.28805c-0.1905,0.18793 -0.29774,0.44436 -0.29774,0.71195h-1.48633c-0.36064,-0.0051 -0.69608,0.18438 -0.87789,0.49587c-0.18181,0.3115 -0.18181,0.69676 0,1.00825c0.18181,0.3115 0.51725,0.50097 0.87789,0.49587h18c0.36064,0.0051 0.69608,-0.18438 0.87789,-0.49587c0.18181,-0.3115 0.18181,-0.69676 0,-1.00825c-0.18181,-0.3115 -0.51725,-0.50097 -0.87789,-0.49587h-1.48633c0,-0.26759 -0.10724,-0.52403 -0.29774,-0.71195c-0.1905,-0.18793 -0.44836,-0.29168 -0.71593,-0.28805h-5.5v-0.5c0.0037,-0.2703 -0.10218,-0.53059 -0.29351,-0.72155c-0.19133,-0.19097 -0.45182,-0.29634 -0.72212,-0.29212zM6,9l1.79297,15.23438c0.118,1.007 0.97037,1.76563 1.98438,1.76563h10.44531c1.014,0 1.86538,-0.75862 1.98438,-1.76562l1.79297,-15.23437z"></path></g></g>
-                </svg>
-          <div id="tooltip-add-to-favorites" role="tooltip" class="tooltip invisible absolute z-10 inline-block rounded-lg bg-gray-900 px-3 py-2 text-sm font-medium text-white opacity-0 shadow-sm transition-opacity duration-300 dark:bg-gray-700" data-popper-placement="top">
-            Remove
-            <div class="tooltip-arrow" data-popper-arrow=""></div>
-          </div>
+                <form method="POST" action="{{ route('products.delete',$product->slug) }}">
+                    @csrf
+                    @method('DELETE')
+
+                    <button type="submit" data-tooltip-target="tooltip-add-to-favorites"
+                        data-modal-target="popup-modal" data-modal-toggle="popup-modal" class="mb-1">
+                        <span class="sr-only"> Delete </span>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0,0,300,150">
+                            <g fill="#cf0c0c" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt"
+                                stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0"
+                                font-family="none" font-weight="none" font-size="none" text-anchor="none"
+                                style="mix-blend-mode: normal">
+                                <g transform="scale(8.53333,8.53333)">
+                                    <path
+                                        d="M14.98438,2.48633c-0.55152,0.00862 -0.99193,0.46214 -0.98437,1.01367v0.5h-5.5c-0.26757,-0.00363 -0.52543,0.10012 -0.71593,0.28805c-0.1905,0.18793 -0.29774,0.44436 -0.29774,0.71195h-1.48633c-0.36064,-0.0051 -0.69608,0.18438 -0.87789,0.49587c-0.18181,0.3115 -0.18181,0.69676 0,1.00825c0.18181,0.3115 0.51725,0.50097 0.87789,0.49587h18c0.36064,0.0051 0.69608,-0.18438 0.87789,-0.49587c0.18181,-0.3115 0.18181,-0.69676 0,-1.00825c-0.18181,-0.3115 -0.51725,-0.50097 -0.87789,-0.49587h-1.48633c0,-0.26759 -0.10724,-0.52403 -0.29774,-0.71195c-0.1905,-0.18793 -0.44836,-0.29168 -0.71593,-0.28805h-5.5v-0.5c0.0037,-0.2703 -0.10218,-0.53059 -0.29351,-0.72155c-0.19133,-0.19097 -0.45182,-0.29634 -0.72212,-0.29212zM6,9l1.79297,15.23438c0.118,1.007 0.97037,1.76563 1.98438,1.76563h10.44531c1.014,0 1.86538,-0.75862 1.98438,-1.76562l1.79297,-15.23437z">
+                                    </path>
+                                </g>
+                            </g>
+                        </svg>
+                        <div id="tooltip-add-to-favorites" role="tooltip"
+                            class="tooltip invisible absolute z-10 inline-block rounded-lg bg-gray-900 px-3 py-2 text-sm font-medium text-white opacity-0 shadow-sm transition-opacity duration-300 dark:bg-gray-700"
+                            data-popper-placement="top">
+                            Remove
+                            <div class="tooltip-arrow" data-popper-arrow=""></div>
+                        </div>
+                    </button>
+                </form>
+
+            </div>
+
+            <p class="text-lg font-extrabold leading-tight text-gray-900 dark:text-white">$ {{ $product?->price }}</p>
+
         </div>
 
-        <p class="text-2xl font-extrabold leading-tight text-gray-900 dark:text-white">$1,699</p>
+        <a href="#" class="text-lg font-semibold leading-tight text-gray-900 hover:underline dark:text-white">
+            {{ $product?->name }}
+        </a>
 
-      </div>
+        <ul class="mt-2 flex items-center gap-4">
+            <li class="flex items-center gap-2">
+                <svg class="h-4 w-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M13 7h6l2 4m-8-4v8m0-8V6a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1v9h2m8 0H9m4 0h2m4 0h2v-4m0 0h-5m3.5 5.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0Zm-10 0a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0Z" />
+                </svg>
+                <p class="text-sm font-medium text-gray-500 dark:text-gray-400">{{ $product?->quantity_available }}</p>
+            </li>
 
-      <a href="#" class="text-lg font-semibold leading-tight text-gray-900 hover:underline dark:text-white">
-        {{ $product?->name }}
-      </a>
-
-      <ul class="mt-2 flex items-center gap-4">
-        <li class="flex items-center gap-2">
-          <svg class="h-4 w-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 7h6l2 4m-8-4v8m0-8V6a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1v9h2m8 0H9m4 0h2m4 0h2v-4m0 0h-5m3.5 5.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0Zm-10 0a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0Z" />
-          </svg>
-          <p class="text-sm font-medium text-gray-500 dark:text-gray-400">{{ $product?->quantity_available }}</p>
-        </li>
-
-      </ul>
+        </ul>
 
     </div>
-  </div>
+</div>
