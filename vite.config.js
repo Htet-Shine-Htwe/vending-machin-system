ab8f1feecdd62506987b4a2060206c3d82314e31
Htet-Shine-Htwe <htetshine.coin@gmail.com>
2024-08-31 17:04:20 +0630

    config : file configured

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,8 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+                'public/js/user/cart.js',
+                'public/js/user/cart-ui.js',
             ],
             refresh: true,
         }),
